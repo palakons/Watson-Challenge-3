20d8e6b49807b3b8549b3f1acd80fcae2c674be9
palakons <palakons@gmail.com>
2017-03-08 02:50:48 +0700

    tone graph stable

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,7 +1,5 @@
-$(document).ready(function(){
-    $('[data-toggle="tooltip"]').tooltip();   
-});
-getHistory('history-content',$('#sourceText')[0].value,'en',$('#destLang')[0].value,'completed-transaction',function(){});
+
+getHistory('history-content', $('#sourceText')[0].value, 'en', $('#destLang')[0].value, 'completed-transaction', function () { });
 
 function useAPI(text, source, dest, output) {
 	console.log('LOCAL use API now');
@@ -15,9 +13,12 @@ function useAPI(text, source, dest, output) {
 	function callback(data, textStatus, jqXHR) {
 		console.log(data);
 		$('#' + output)[0].innerHTML = '';
-		$('#' + output)[0].innerHTML += '(' + data.sourceLanguage + ')' + data.Translation.sourceText;
-		$('#' + output)[0].innerHTML += '<div id="toneGraph"></div>';
-		$('#' + output)[0].innerHTML += '(' + data.destinationLanguage + ')' + data.Translation.translatedText;
+		$('#' + output)[0].innerHTML += '<p><a href="#" data-toggle="tooltip" title="' + data.sourceLanguage + '"><span class="badge">' + data.Translation.sourceLanguageCode + '</span></a> ' + data.Translation.sourceText+'<p>';
+		$('#' + output)[0].innerHTML += '<p>&#8659;</p>';
+		$('#' + output)[0].innerHTML += '<p><a href="#" data-toggle="tooltip" title="' + data.destinationLanguage + '"><span class="badge">' + data.Translation.destinationLanguageCode + '</span></a> ' + data.Translation.translatedText+'<p>';
+		$('#' + output)[0].innerHTML += '<div id="toneGraph" style="height:200px;"></div>';
+		$('[data-toggle="tooltip"]').tooltip();
+		plotToneGraph(data, 'toneGraph');
 	}
 
 	$.get(url, reqData, callback);
@@ -32,18 +33,22 @@ function getHistory(output, ttext, source, dest, output2, callback_tran) {
 		console.log(ttext, source, dest, output2);
 		callback_tran(ttext, source, dest, output2);
 		console.log(data, textStatus, jqXHR);
-		var text = '<table class = "table"><thead><tr><th>No.</th><th>Original</th><th>Target Language</th><th>Translated</th></tr></thead><tbody>';
+		var text = '<table class = "table"><thead><tr><th>No.</th><th>Original</th><th>Translated</th></tr></thead><tbody>';
 
 		for (var i in data) {
 			var j = parseInt(i) + 1;
-			text += '<tr><td>' + (j == 1 ? '<mark>recent</mark>' : j) + '</td><td>' + data[i].Translation.sourceText + '</td><td>' + data[i].Translation.destinationLanguage + '</td><td><a href="#" data-toggle="tooltip" title="'+data[i].Translation.destinationLanguage+'"><span class="badge">'+data[i].Translation.destinationLanguageCode+'</span></a>' + data[i].Translation.translatedText + '</td></tr>';
+			text += '<tr><td>' + (j == 1 ? '<mark>recent</mark>' : j) + '</td><td>' + data[i].Translation.sourceText + '</td><td><a href="#" data-toggle="tooltip" title="' + data[i].Translation.destinationLanguage + '"><span class="badge">' + data[i].Translation.destinationLanguageCode + '</span></a> ' + data[i].Translation.translatedText + '</td></tr>';
 		}
 		text += '</tbody></table>';
 		$('#' + output)[0].innerHTML = text;
+		$('[data-toggle="tooltip"]').tooltip();
+
 	}
 	$.get(url, reqData, callback);
 }
-function plotToneGraph(translation) {
+function plotToneGraph(translation, toneDiv) {
+	console.log('translation data');
+	console.log(translation);
 	/*translation.sourceTextTone
 	translation.translatedTextTone
 		.document_tone.tone_categories[].category_id === 'social_tone'
@@ -51,24 +56,27 @@ function plotToneGraph(translation) {
 			.document_tone.tone_categories[].tones[].tone_name
 			.document_tone.tone_categories[].tones[].score*/
 	var toneData = [];
-	var tempTone = translation.sourceTextTone;
+	var tempTone = translation.Translation.sourceTextTone;
 	for (var i in tempTone.document_tone.tone_categories) {
-		if (tempTone.document_tone.tone_categories.category_id === 'social_tone') {
+		if (tempTone.document_tone.tone_categories[i].category_id === 'social_tone') {
 			tempTone.document_tone.tone_categories[i].tones.sort(function (a, b) {
 				if (a.tone_name < b.tone_name) return -1;
 				if (a.tone_name > b.tone_name) return 1;
 				return 0;
 			});
 			for (var j in tempTone.document_tone.tone_categories[i].tones) {
+				toneData[j] = {};
 				toneData[j]["category"] = tempTone.document_tone.tone_categories[i].tones[j].tone_name;
 				toneData[j]["column-1"] = tempTone.document_tone.tone_categories[i].tones[j].score;
 			}
+			break;
+			
 		}
-		break;
 	}
-	var tempTone = translation.translatedTextTone;
+	var tempTone = translation.Translation.translatedTextTone;
 	for (var i in tempTone.document_tone.tone_categories) {
-		if (tempTone.document_tone.tone_categories.category_id === 'social_tone') {
+		console.log(tempTone.document_tone.tone_categories[i].category_id);
+		if (tempTone.document_tone.tone_categories[i].category_id === 'social_tone') {
 			tempTone.document_tone.tone_categories[i].tones.sort(function (a, b) {
 				if (a.tone_name < b.tone_name) return -1;
 				if (a.tone_name > b.tone_name) return 1;
@@ -77,17 +85,18 @@ function plotToneGraph(translation) {
 			for (var j in tempTone.document_tone.tone_categories[i].tones) {
 				toneData[j]["column-2"] = tempTone.document_tone.tone_categories[i].tones[j].score;
 			}
+			break;
 		}
-		break;
 	}
 	console.log(toneData);
-	AmCharts.makeChart("toneGraph",
+	AmCharts.makeChart(toneDiv,
 		{
 			"type": "serial",
 			"categoryField": "category",
 			"startDuration": 1,
 			"handDrawn": true,
 			"categoryAxis": {
+				"labelRotation": -10,
 				"gridPosition": "start"
 			},
 			"trendLines": [],
@@ -122,13 +131,7 @@ function plotToneGraph(translation) {
 				"enabled": true,
 				"useGraphSettings": true
 			},
-			"titles": [
-				{
-					"id": "Title-1",
-					"size": 15,
-					"text": "Chart Title"
-				}
-			],
+			"titles": [],
 			"dataProvider": toneData
 			/*[
 				{
